0b6bb950520bb31b81a001be9c90574521809fcb
Vickson Almeida <127789476+Vickson-Almeida@users.noreply.github.com>
2023-07-20 13:37:56 -0400

    Inclusão de Aulas de Java Scripts

---
+++ JavaScript/Aulas JavaScript/_scripts/script_inicial.js
@@ -0,0 +1,9 @@
+//alert - Emitir um alerta no topo da página como uma pop-up
+alert('Olá Iniciou no JavaScript!')
+/*
+Comentário de multiplas linhas
+
+getElementById - pegar um valor de um input pelo Id 
+                que está nele
+*/
+document.getElementById('nome').value = 'Oi!'
